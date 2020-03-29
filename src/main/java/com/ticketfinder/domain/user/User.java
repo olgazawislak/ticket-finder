09d717561d5d2c481dc4e4246d88ece100c69a11
Olga <olgazawislak@gmail.com>
2020-03-29 23:33:52 +0200

    #17 Add Log in feature

--- src/main/java/com/ticketfinder/domain/user/User.java
+++ src/main/java/com/ticketfinder/domain/user/User.java
@@ -1,9 +1,11 @@
 package com.ticketfinder.domain.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
--- src/main/java/com/ticketfinder/domain/user/UserController.java
+++ src/main/java/com/ticketfinder/domain/user/UserController.java
@@ -1,12 +1,11 @@
 package com.ticketfinder.domain.user;
 
 import com.ticketfinder.exception.BadRequestException;
+import com.ticketfinder.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -21,9 +20,24 @@ public class UserController {
     @PostMapping("users")
     @ResponseStatus(code = HttpStatus.CREATED)
     public void createUser(@RequestBody CreateUserCommand createUserCommand) {
+        if (userRepository.existsById(createUserCommand.getEmail())) {
+            throw new BadRequestException("An account for this e-mail already exist");
+        }
         if (!createUserCommand.isEmailValid()) {
             throw new BadRequestException("Incorrect e-mail address");
         }
         userRepository.save(createUserCommand.toUser());
+    }
+
+    @PostMapping("login")
+    public void logIn(@RequestBody CreateUserCommand createUserCommand ) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashPassword = userRepository.findById(createUserCommand.getEmail())
+                .orElseThrow(NotFoundException::new)
+                .getHashPassword();
+
+        if(!passwordEncoder.matches(createUserCommand.getPassword(), hashPassword)) {
+            throw new BadRequestException("Incorrect e-mail or password");
+        }
     }
 }
